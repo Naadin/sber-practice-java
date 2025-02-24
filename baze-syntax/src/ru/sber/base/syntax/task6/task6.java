f3e13a50e25b4a968ccec23e1fb4d3e1c8ba101b
Naadinn <nadya.bezgodova922@gmail.com>
2025-02-24 23:07:14 +0300

    Выполнено 6 задание - Случайные числа.

---
+++ baze-syntax/src/ru/sber/base/syntax/task6/task6.java
@@ -0,0 +1,25 @@
+package ru.sber.base.syntax.task6;
+import java.util.ArrayList; // динамический массив
+import java.util.Random;
+public class task6 {
+    private static final int size = 100;
+    public static void main(String[] args) {
+        ArrayList<Integer> array = new ArrayList<>(size); // контейнер для хранения элементов
+
+        double y = 0.0;
+        int max = Integer.MIN_VALUE;
+        int maxIndex = 1;
+        for (int i = 0; i < size; ++i) {
+            array.add((int) (Math.random() * 1001)); // заполнение массива
+            if (max < array.get(array.size() - 1)) { // поиск максимума
+                maxIndex = i;
+                max = array.get(array.size() - 1);
+            }
+            y += array.get(array.size() - 1);
+        }
+        y /= size; // поиск среднего значения
+        System.out.println("Массив: " + array); // вывод массива на экран
+        System.out.println("Максимальное значение = [" + max + "], находится в элементе под индексом = [" + maxIndex + "].");
+        System.out.println("Среднее значение = ["  + y + "].");
+    }
+}
