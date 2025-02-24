8df587260bed1de7cd929944bf81aa16bd0f9992
Naadinn <nadya.bezgodova922@gmail.com>
2025-02-25 00:03:03 +0300

    Выполнено 10 задание - "Привет".

--- baze-syntax/src/ru/sber/base/syntax/task10/task10.java
+++ baze-syntax/src/ru/sber/base/syntax/task10/task10.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class task10 {
     public static void main(String[] args) {
         System.out.print("Введите ваше имя: ");
-        String name = new Scanner(System.in).next()ж
+        String name = new Scanner(System.in).next();
 
         javax.swing.JOptionPane.showMessageDialog(null, "Привет, " + name);
     }
