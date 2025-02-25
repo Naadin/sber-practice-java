e9d137023ff35ff6b51ea04df99f8a06229dff08
Naadinn <nadya.bezgodova922@gmail.com>
2025-02-25 07:42:38 +0300

    Выполнено 8 задание - Сортировка пузырьком.

--- baze-syntax/src/ru/sber/base/syntax/task8/task8.java
+++ baze-syntax/src/ru/sber/base/syntax/task8/task8.java
@@ -1,4 +1,23 @@
 package ru.sber.base.syntax.task8;
-
+import java.util.Arrays; // динамический массив
 public class task8 {
+    public static void main(String[] args) {
+        int[] array = new int[30];
+
+        for (int i = 0; i < array.length; ++i) {
+            array[i] = (int) (Math.random() * 1001); // заполнение массива
+        }
+        System.out.println("Массив: " + Arrays.toString(array)); // вывод массива на экран
+        for (int i = array.length - 1; i >= 0; --i) {
+            for (int j = 0; j < i; ++j) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println("        Сортировка методом пузырька...");
+        System.out.println("Отсортированный массив: " + Arrays.toString(array));
+    }
 }
